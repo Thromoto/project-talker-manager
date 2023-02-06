ce08c09c615b6980ae747db2e774e67fb1731880
Thiago Omoto <thi.omoto@gmail.com>
2023-02-06 19:10:58 -0300

    arrumando o req4 que quebrava o req3

--- src/validationLogin.js
+++ src/validationLogin.js
@@ -1,4 +1,6 @@
-module.exports = (req, res) => {
+const validationRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/i;
+
+module.exports = (req, res, next) => {
     const { email, password } = req.body;
     if (!email) {
         return res.status(400).json({ message: 'O campo "email" é obrigatório' });
@@ -9,7 +11,8 @@ module.exports = (req, res) => {
       if (password.length < 6) {
         return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
       }
-      if (email !== /\S+@\S+\.\S+/) {
+      if (!validationRegex.test(email)) {
         return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
       }
+      next();
 };
