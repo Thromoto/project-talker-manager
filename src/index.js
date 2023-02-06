83f71659f28a5525b1d6e4d33f600dff70b30973
Thiago Omoto <thi.omoto@gmail.com>
2023-02-06 15:40:21 -0300

    requisito 1 ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+const talkerPath = path.resolve(__dirname, './talker.json');
+
+const readFile = async () => {
+  try {
+    const data = await fs.readFile(talkerPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(`Arquivo não pode ser lido: ${error}`);
+  }
+}
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -14,4 +27,12 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
-// ...
+
+app.get('/talker', async (req, res) => {
+  try {
+    const talker = await readFile();
+    res.status(200).json(talker);
+  } catch (err) {
+    res.status(200).send({ err });
+  }
+});
