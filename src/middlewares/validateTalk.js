6c16becd52ee268d7725b078102f58550a689d31
Thiago Omoto <thi.omoto@gmail.com>
2023-02-08 12:03:26 -0300

    requisito 5 e 6 ok

--- src/middlewares/validateTalk.js
+++ src/middlewares/validateTalk.js
@@ -6,6 +6,9 @@ module.exports = (req, res, next) => {
     if (!talk.watchedAt) {
         return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
     }
+    if (Number(talk.rate) === 0) {
+        return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+      }
     if (!talk.rate) {
         return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
     }
